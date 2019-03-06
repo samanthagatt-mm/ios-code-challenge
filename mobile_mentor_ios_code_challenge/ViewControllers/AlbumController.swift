c1da2dde3e0294c6dc9a1acf64d23c7a1b917cf0
Samantha Gatt <smfgatt@gmail.com>
2019-03-06 13:51:24 -0500

    Displays album song titles

--- mobile_mentor_ios_code_challenge/ViewControllers/AlbumController.swift
+++ mobile_mentor_ios_code_challenge/ViewControllers/AlbumController.swift
@@ -8,6 +8,35 @@
 
 import UIKit
 
-class AlbumController: UIViewController {
+class AlbumController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    var tableView: UITableView = { return UITableView() }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor().HexToColor(hexString: "#323F44", alpha: 1)
+        tableView.backgroundColor = UIColor().HexToColor(hexString: "#323F44", alpha: 1)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "SongCell")
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        Constraints().constraintWithTopAndLeadingAnchor(field: tableView, width: view.safeAreaLayoutGuide.layoutFrame.width, height: view.safeAreaLayoutGuide.layoutFrame.height, topAnchor: view.safeAreaLayoutGuide.topAnchor, topConstant: 0.0, leadingAnchor: view.safeAreaLayoutGuide.leadingAnchor, leadingConstant: 0.0)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return AlbumViewModel.songs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SongCell", for: indexPath)
+        cell.backgroundColor = UIColor().HexToColor(hexString: "#323F44", alpha: 1)
+        cell.textLabel?.textColor = UIColor().HexToColor(hexString: "#50A3E3", alpha: 1)
+        let song = AlbumViewModel.songs[indexPath.row]
+        cell.textLabel?.text = "\(song.trackNumber ?? 0). \(song.trackName ?? "")"
+        return cell
+    }
 }
